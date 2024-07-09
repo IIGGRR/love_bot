6560330f532e9747fe346f0eaeddd58242b3125d
IIGGRR <igo1742000@gmail.com>
2024-07-09 18:15:40 +0300

    step_one_one

--- databasa/models.py
+++ databasa/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, ForeignKey, Integer
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 import os
@@ -15,14 +15,14 @@ class Base(AsyncAttrs, DeclarativeBase):
 
 class User(Base):
     __tablename__ = 'users'
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     tg_id: Mapped[str] = mapped_column(String(32))
     tg_fr_id: Mapped[str] = mapped_column(String(32))
 
 
 class Photo(Base):
     __tablename__ = 'photos'
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     file_path: Mapped[str] = mapped_column(String(256))
     user_id: Mapped[str] = mapped_column(ForeignKey('users.id'))
 
--- databasa/requests.py
+++ databasa/requests.py
@@ -11,7 +11,7 @@ async def set_user(tg_id, tg_fr_id):
 
 async def check_registration(tg_id):
     async with async_session() as session:
-        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        user = await session.scalar(select(User).where(User.tg_id == int(tg_id)))
         if user:
             return False
         else:
@@ -20,7 +20,7 @@ async def check_registration(tg_id):
 
 async def get_id_partner(tg_id):
     async with async_session() as session:
-        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        user = await session.scalar(select(User).where(User.tg_id == int(tg_id)))
         return user.tg_fr_id
 
 
@@ -32,13 +32,13 @@ async def set_photo(file_path, user_id):
 
 async def get_photo(photo_id):
     async with async_session() as session:
-        photo = await session.scalar((select(Photo).where(photo_id == Photo.id)))
+        photo = await session.scalar((select(Photo).where(int(photo_id) == Photo.id)))
         return photo
 
 
 async def get_all_photo_partner(user_id):
     async with async_session() as session:
-        photos = await session.scalars((select(Photo).where(Photo.user_id == user_id)))
+        photos = await session.scalars((select(Photo).where(Photo.user_id == int(user_id))))
         return photos
 
 
