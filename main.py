f004528a183c3312e530009886fc0f262d2afd2f
lolovski <alexpaxyaev@gmail.com>
2024-08-25 12:44:20 +0300

    alembic init

--- main.py
+++ main.py
@@ -18,17 +18,17 @@ bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTM
 async def main() -> None:
     # wait async_main()
     # await add_visits()
-    from love_bot.handlers.basic import router as basic_router
+    from handlers.basic import router as basic_router
     dp.include_router(basic_router)
-    from love_bot.handlers.help_handler import router as help_router
+    from handlers.help_handler import router as help_router
     dp.include_router(help_router)
-    from love_bot.handlers.photo import router as photo_router
+    from handlers.photo import router as photo_router
     dp.include_router(photo_router)
-    from love_bot.handlers.registration import router as registration_router
+    from handlers.registration import router as registration_router
     dp.include_router(registration_router)
-    from love_bot.handlers.profile import router as profile_router
+    from handlers.profile import router as profile_router
     dp.include_router(profile_router)
-    from love_bot.handlers.roleplay import router as roleplay_router
+    from handlers.roleplay import router as roleplay_router
     dp.include_router(roleplay_router)
     await dp.start_polling(bot)
 
